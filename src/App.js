e56ec09bbe7f0e7e58144f5ed9b8094cc873bcfa
Salik16 <salimkarabashev86@gmail.com>
2023-05-18 21:12:57 +0600

    My Changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import MainRoutes from "./routes/MainRoutes";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <MainRoutes />
+    </div>
+  );
 }
 
 export default App;
